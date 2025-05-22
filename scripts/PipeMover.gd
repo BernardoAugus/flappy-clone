f7ac1cb6498e8b4153823d1fbf4d61a41b0c7b53
BernardoAugus <bernardo.11salgado@hotmail.com>
2025-05-22 00:16:50 -0300

    chore: ajusta posição inicial e intervalo de geração dos canos

--- scripts/PipeMover.gd
+++ scripts/PipeMover.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var speed := 100.0
+@export var speed: float = 100.0
 
 func _process(delta):
 	position.x -= speed * delta
--- scripts/PipeSpawner.gd
+++ scripts/PipeSpawner.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var pipe_scene: PackedScene
-@export var spawn_interval: float = 2.0
+@export var spawn_interval: float = 7.0
 @export var pipe_speed: float = 100.0
 @export var vertical_range: float = 150.0
 
@@ -19,7 +19,7 @@ func spawn_pipe_pair():
 	get_parent().add_child(pipe_instance)
 
 	# Começa fora da tela (ajuste conforme tamanho da tela)
-	pipe_instance.position.x = 400  # direita da tela (ajuste se sua tela for maior)
+	pipe_instance.position.x = 1000  # direita da tela (ajuste se sua tela for maior)
 	pipe_instance.position.y = randf_range(-vertical_range, vertical_range)
 
 	# Adiciona script de movimento
